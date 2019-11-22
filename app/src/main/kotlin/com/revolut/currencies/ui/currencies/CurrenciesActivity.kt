e2b73dd85546f8763af79efa2bc1cc55a7c1cab8
Alfred <a.afutu@mytaxi.com>
2019-11-22 17:16:41 +0100

    Update Config and Renaming
    * Updated gradle dependencies, refactored files and directories

--- app/src/main/kotlin/com/revolut/currencies/ui/currencies/CurrenciesActivity.kt
+++ app/src/main/kotlin/com/revolut/currencies/ui/currencies/CurrenciesActivity.kt
@@ -1,17 +1,17 @@
-package com.revolut.currencies
+package com.revolut.currencies.ui.currencies
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.revolut.currencies.ui.main.MainFragment
+import com.revolut.currencies.R
 
-class MainActivity : AppCompatActivity() {
+class CurrenciesActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.main_activity)
         if (savedInstanceState == null) {
             supportFragmentManager.beginTransaction()
-                .replace(R.id.container, MainFragment.newInstance())
+                .replace(R.id.container, CurrenciesFragment.newInstance())
                 .commitNow()
         }
     }
---
+++ app/src/main/kotlin/com/revolut/currencies/ui/currencies/CurrenciesViewModel.kt
@@ -0,0 +1,7 @@
+package com.revolut.currencies.ui.currencies
+
+import androidx.lifecycle.ViewModel
+
+class CurrenciesViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
